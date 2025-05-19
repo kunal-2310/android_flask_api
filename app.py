631ecf2aed451bd60f68a1f942f43992f30fdaa0
kunal-2310 <kunal.23102000@gmail.com>
2025-05-19 16:42:21 +0530

    first commit

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def process_prompt():
     return jsonify({"answer": result_json})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
