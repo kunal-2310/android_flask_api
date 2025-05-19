814ea0646a2126cf79c8c211f1abce0e48c435e5
kunal-2310 <kunal.23102000@gmail.com>
2025-05-19 17:34:56 +0530

    second commit

--- app.py
+++ app.py
@@ -22,10 +22,10 @@ def process_prompt():
 
     # Define a prompt template
     extract_fields = ChatPromptTemplate.from_messages([
-        ("system", "You are a good assistant, help me in extracting some data from prompt that is provided."),
-        ("human", """I will provide you a prompt like 'Assign a high-priority AC maintenance task. 
-    The site is a customer named Rajesh Sharma. Schedule it for tomorrow at 11 AM. and duration of the task is 2 hours. location is sector 21,Gurgaon'
-    then you have to give me the response in JSON format like:
+        ("system", "You are a good assistant, help me in extracting some data from the prompt that is provided."),
+        ("human", """This is just sample data solely meant to understand the working do not copy the data mentioned in this example:
+    I will provide you a prompt like 'Assign a high-priority AC maintenance task. The site is a customer named Rajesh Sharma. Schedule it for tomorrow at 11 AM. and duration of the task is 2 hours. location is sector 21,Gurgaon'
+    It is your job to analyse the initial prompt and give me the response in JSON format like:
     {{
     "description": "AC Maintenance",
     "location": "Sector 21, Gurgaon",
@@ -34,10 +34,11 @@ def process_prompt():
     "endTime": "16-05-2025 1:00 pm",
     "customerName": "Rajesh Sharma"
     }}"""),
-    ("human","important -> donot add extra fields in response other than fields specified by me"),
+    ("human","important -> do not add extra fields in response other than fields specified by me"),
         ("human", "\n\n\n\n do not generate the output based on the above provided sample that is only for reference how to generate response \
-\n\n\nnow the actual prompt is '{prompt}' and if any field data is missing than keep it blank \
-\n\n\n if there is nothing provided in quotes '' after actual prompt keyword than keep the structure of response same and leave the values blank for eg- 'name':'',etc ")
+\n\n\nnow the actual prompt is '{prompt}' and if any field data is missing then keep it blank \
+\n\n\n if there is nothing provided in quotes '' after actual prompt keyword then keep the structure of the response same and leave the values blank for eg- 'name':''\
+\n\n\n assign the priority on the basis of the urgency stated in the initial prompt accordingly to one of these- ['Low','Normal','High','Critical'])
     ])
 
 
